e5cf977f574d9da7aecfb311271d2c829d2ddd2a
ArshadFauzil <mohomedarshad1@gmail.com>
2024-03-10 10:44:38 -0400

    repo changes

--- TaskManager/Repository/IUserTaskRepository.cs
+++ TaskManager/Repository/IUserTaskRepository.cs
@@ -1,7 +1,10 @@
 
+using System;
+
 namespace TaskManager.Repository;
 
 public interface IUserTaskRepository
 {
-    
+    public List<UserTaskDataModel> getAllUserTasks();
+    public UserTaskDataModel getUserTaskById(Guid id);
 }
--- TaskManager/Repository/UserTaskRepository.cs
+++ TaskManager/Repository/UserTaskRepository.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
+
 namespace TaskManager.Repository;
 
 public class UserTaskRepository : IUserTaskRepository
 {
-
+    
 }
